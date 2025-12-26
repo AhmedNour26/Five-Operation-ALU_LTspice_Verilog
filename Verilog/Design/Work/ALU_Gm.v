32f5c57a4aec2d6767b9e583ecfd1be70736f911
ahmed nour <110340657+anour1@users.noreply.github.com>
2025-12-26 18:19:18 +0200

    ver and reporting

--- Verilog/Design/Work/ALU_Gm.v
+++ Verilog/Design/Work/ALU_Gm.v
@@ -8,22 +8,22 @@ module ALU_Gm(
     output Sign_F
 );
 
-    reg signed [7:0] C_reg;
+    reg signed [7:0] C_temp;
 
     always @(*) begin
         case (Sel)
-            0: C_reg = A + B;                 
-            1: C_reg = A - B;                 
-            2: C_reg = A * B;                 
-            3: C_reg = (B == 0) ? 8'sd0 : A % B; 
-            4: C_reg = A >>> B;        
-            default: C_reg = 0;   
+            0: C_temp = A + B;                 
+            1: C_temp = A - B;                 
+            2: C_temp = A * B;                 
+            3: C_temp = (B == 0) ? 8'sd0 : A % B; 
+            4: C_temp = A >>> B;        
+            default: C_temp = 0;   
         endcase
     end
 
-    assign C        = C_reg;
-    assign Zero_F   = (C_reg == 0);
+    assign C        = C_temp;
+    assign Zero_F   = (C_temp == 0);
     assign D_Zero_F = (Sel == 3 && B == 0);
-    assign Sign_F   = C_reg[7];
+    assign Sign_F   = C_temp[7];
 
 endmodule
